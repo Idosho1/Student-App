51e9458479423a77869f89da05f1913432a9f4e9
Idosho1 <ido.shoshani1@gmail.com>
2021-03-02 14:07:27 -0500

    Initial Commit

---
+++ Shared/Student_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Student_AppApp.swift
+//  Shared
+//
+//  Created by Ido Shoshani on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Student_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
